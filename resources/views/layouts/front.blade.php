0be7a35c8606402f3e545fdf24df1cda1a510926
nadjatarassovaee <nadjatarassova@gmail.com>
2025-10-04 05:35:42 +0300

    was added footer - front

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -231,119 +231,63 @@
                 @yield('content')
 	
             <!--Footer-->
-            <footer class="site-footer style-2" id="footer">
-                <div class="footer-bg-wrapper" id="app-banner">
-                    <div class="footer-top">
-                        <div class="container">
-                            <div class="footer-subscribe-wrapper">
-                                <div class="wrapper-inner">
-                                    <div class="row justify-content-between">
-                                        <div class="col-xl-4 col-lg-4 m-lg-0 m-b20 wow fadeInUp" data-wow-delay="0.4s">
-                                            <div class="footer-logo">
-                                                <a href="{{ url('/') }}">
-                                                    <img src="{{ asset('assets/images/logopng.png') }}" alt="logo">
-                                                </a>
-                                            </div>
-                                            <p class="text-white mb-0 font-14">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                                        </div>
-                                        <div class="col-xl-6 col-lg-6 wow fadeInUp" data-wow-delay="0.6s">
-                                            <h4 class="text-white title m-b15">Subscribe To Our Newsletter</h4>
-                                            <form class="dzSubscribe" action="{{ asset('assets/script/mailchamp.php') }}" method="post">
-                                                <div class="dzSubscribeMsg text-white"></div>
-                                                <div class="input-group">
-                                                    <input name="dzEmail" required="required" type="text" class="form-control" placeholder="Enter Your Email">
-                                                    <div class="input-group-addon">
-                                                        <button name="submit" value="submit" type="submit" class="btn btn-primary btn-hover-2">
-                                                            <span>Subscribe</span><i class="fa-solid fa-paper-plane"></i>
-                                                        </button>
-                                                    </div>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 wow fadeInUp" data-wow-delay="0.4s">
-                                    <div class="widget widget_getintuch">
-                                        <h5 class="footer-title">Contact</h5>
-                                        <ul>
-                                            <li>
-                                                <i class="flaticon-placeholder"></i>
-                                                <p>1247/Plot No. 39, 15th Phase, Colony, Kkatpally, Hyderabad</p>
-                                            </li>
-                                            <li>
-                                                <i class="flaticon-telephone"></i>
-                                                <p>+91 987-654-3210<br>
-                                                    +91 123-456-7890</p>
-                                            </li>
-                                            <li>
-                                                <i class="flaticon-email-1"></i>
-                                                <p>info@example.com<br>
-                                                info@example.com</p>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="col-xl-3 col-lg-2 col-md-6 col-sm-6">
-                                    <div class="widget widget_services">
-                                        <h5 class="footer-title">Our Links</h5>
-                                        <ul>
-                                            <li><a href="index.html"><span>Home</span></a></li>
-                                            <li><a href="about-us.html"><span>About Us</span></a></li>
-                                            <li><a href="services.html"><span>Services</span></a></li>
-                                            <li><a href="team.html"><span>Team</span></a></li>
-                                            <li><a href="blog-standard.html"><span>Blog</span></a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
-                                    <div class="widget widget_services">
-                                        <h5 class="footer-title">OUR SERVICES</h5>
-                                        <ul>
-                                            <li><a href="blog-open-gutenberg.html"><span>Strategy & Research</span></a></li>
-                                            <li><a href="services.html"><span>Fast Delivery</span></a></li>
-                                            <li><a href="contact-us.html"><span>Seat Reservation</span></a></li>
-                                            <li><a href="shop-style-1.html"><span>Pickup In Store</span></a></li>
-                                            <li><a href="our-menu-1.html"><span>Our Menu</span></a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6	">
-                                    <div class="widget widget_services">
-                                        <h5 class="footer-title">Help Center</h5>
-                                        <ul>
-                                            <li><a href="faq.html"><span>FAQ</span></a></li>
-                                            <li><a href="shop-style-1.html"><span>Shop</span></a></li>
-                                            <li><a href="shop-style-2.html"><span>Category Filter</span></a></li>
-                                            <li><a href="testimonial.html"><span>Testimonials</span></a></li>
-                                            <li><a href="contact-us.html"><span>Contact Us</span></a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+            <footer id="footer" class="uh-micro-footer">
+                <div class="container py-3 py-md-4">
+                  <div class="row g-3 align-items-end">
+                    <!-- ЛОГО -->
+                    <div class="col-12 col-md-4 text-center text-md-start">
+                      <a href="{{ url('/') }}" class="uh-brand d-inline-flex align-items-center gap-2">
+                        <img src="{{ asset('assets/images/logopng.png') }}" alt="Uued Horisondid" style="height:72px" loading="lazy">
+                      </a>
                     </div>
-                </div>
-                <!-- Footer Bottom Part -->
-                <div class="container">
-                    <div class="footer-bottom">
-                        <div class="row">
-                            <div class="col-xl-6 col-lg-6">
-                                <span class="copyright-text">Crafted With <span class="heart"></span> by <a href="https://dexignzone.com/" target="_blank">DexignZone</a></span>
-                            </div>
-                            <div class="col-xl-6 col-lg-6">
-                                <ul class="footer-link">
-                                    <li><a href="blog-standard.html">Blog Detail</a></li>
-                                    <li><a href="about-us.html">About</a></li>
-                                    <li><a href="testimonial.html">Testimonials</a></li>
-                                </ul>
-                            </div>
-                        </div>
+              
+                    <!-- МЕНЮ -->
+                    <div class="col-12 col-md-4">
+                      <ul class="uh-nav list-unstyled d-flex justify-content-center flex-wrap gap-2 gap-md-3 mb-0">
+                        <li><a href="{{ route('home') }}">Главная</a></li>
+                        <li><a href="{{ route('events.index') }}">Мероприятия</a></li>
+                        <li><a href="{{ route('blog.index') }}">Новости</a></li>
+                        <li><a href="{{ route('contact') }}">Контакты</a></li>
+                      </ul>
                     </div>
+              
+                    <!-- КОНТАКТЫ + IBAN + СОЦСЕТИ -->
+                    <div class="col-12 col-md-4 text-center text-md-end">
+                      <div class="uh-contacts small d-flex flex-wrap justify-content-center justify-content-md-end gap-2">
+                        <a href="tel:+37254290030" class="uh-link"><i class="fa-solid fa-phone"></i> +372 54290030</a>
+                        <a href="mailto:uuedhorisondidtallinn@gmail.com" class="uh-link">
+                          <i class="fa-solid fa-envelope"></i> uuedhorisondidtallinn@gmail.com
+                        </a>
+                      </div>
+              
+                      <!-- IBAN -->
+                      <div class="uh-iban mt-2">
+                        <div class="small text-muted">Счёт для переводов</div>
+                        <div class="small text-muted">Получатель: MTÜ Uued Horisondid</div>
+                        <div class="uh-iban-box d-inline-flex align-items-center gap-2 mt-1">
+                            <span class="uh-copy-msg" aria-live="polite">Скопировано</span><button type="button" class="uh-copy" data-copy="#ibanText" aria-label="Скопировать IBAN" title="Скопировать IBAN">
+                                <i class="fa-regular fa-copy"></i>
+                            </button>
+                          <span class="uh-iban-code" id="ibanText">EE087700771011884557</span>
+                        </div>
+                      </div>
+              
+                      <div class="uh-social mt-2 d-flex justify-content-center justify-content-md-end gap-2">
+                        <a class="fab fa-facebook-f"  target="_blank" href="https://www.facebook.com/share/161aHLsUuT/" aria-label="Facebook"></a>
+                        <a class="fab fa-instagram"   target="_blank" href="https://www.instagram.com/keelekohvik?utm_source=qr&igsh=MWYxN3BqbjNwOGxwcA==" aria-label="Instagram"></a>
+                        <a class="fab fa-telegram"    target="_blank" href="https://t.me/Keeleklubi" aria-label="Telegram"></a>
+                      </div>
+                    </div>
+                  </div>
+              
+                  <hr class="uh-line my-3">
+              
+                  <div class="d-flex flex-column flex-md-row justify-content-between align-items-center small text-muted gap-2">
+                    <span>© {{ date('Y') }} Uued Horisondid MTÜ</span>
+                    <span>Сайт разработан QuickCode OÜ</span>
+                  </div>
                 </div>
             </footer>
-            <!-- Footer -->
             
             <div class="scroltop-progress scroltop-primary">
                 <svg width="100%" height="100%" viewBox="-1 -1 102 102">
@@ -380,6 +324,42 @@
             new dzSettings(dzSettingsOptions);
         });
     </script>
+    <script>
+        document.addEventListener('click', function(e){
+            const btn = e.target.closest('.uh-copy');
+            if(!btn) return;
+        
+            const sel = btn.getAttribute('data-copy');
+            const el  = document.querySelector(sel);
+            if(!el) return;
+        
+            const text = (el.textContent || '').trim();
+            const msg  = btn.parentElement.querySelector('.uh-copy-msg');
+        
+            const done = ()=>{
+            btn.classList.add('copied');
+            if(msg){
+                msg.classList.add('show');
+                msg.textContent = 'Скопировано';
+            }
+            setTimeout(()=>{
+                btn.classList.remove('copied');
+                if(msg) msg.classList.remove('show');
+            }, 1500);
+            };
+        
+            if (navigator.clipboard?.writeText) {
+            navigator.clipboard.writeText(text).then(done).catch(()=>{});
+            } else {
+            const r = document.createRange(); r.selectNodeContents(el);
+            const s = window.getSelection(); s.removeAllRanges(); s.addRange(r);
+            try { document.execCommand('copy'); done(); } catch(_) {}
+            s.removeAllRanges();
+            }
+        });
+    </script>
+        
+        
     @stack('scripts')
 </body>
 </html>
